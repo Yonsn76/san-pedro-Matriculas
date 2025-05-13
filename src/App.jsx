a84e065ec15b64ae55c1448a8ce7f2dcbc1b424d
TomsonLu <retobloyons@gmail.com>
2025-05-13 01:48:48 -0500

    Chabot implemntado

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import MatriculaSection from './sections/MatriculaSection'
 import Testimonios from './sections/Testimonios'
 import Blog from './sections/Blog'
 import Footer from './components/Footer'
+import Chatbot2 from './components/Chatbot2'
 import Form from './pages/matricula/index.jsx'
 import ConsejosPadres from './pages/Consejos_PAdres'
 import NibelacionRecuperacion from './pages/Nibelacion-recuperacion'
@@ -15,46 +16,50 @@ import CursosVacacionales from './pages/Cursos-Vacacionales'
 const App = () => {
   const location = useLocation()
   useEffect(() => {
-    window.scrollTo(0, 0); 
-  }, [location.pathname]); 
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return (
-    <Routes>
-      <Route path="/matricula" element={<Form />} />
-      <Route path="/consejos-padres" element={
-        <>
-          <Header />
-          <ConsejosPadres />
-          <Footer />
-        </>
-      } />
-      <Route path="/nivelacion" element={
-        <>
-          <Header />
-          <NibelacionRecuperacion />
-          <Footer />
-        </>
-      } />
-      <Route path="/cursos-vacacionales" element={
-        <>
-          <Header />
-          <CursosVacacionales />
-          <Footer />
-        </>
-      } />
-      <Route path="/" element={
-        <>
-          <Header />
-          <Hero />
-          <ConsultasGrid />
-          <MatriculaSection />
-          <Testimonios />
-          <Blog />
-          <Footer />
-        </>
-      } />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/matricula" element={<Form />} />
+        <Route path="/consejos-padres" element={
+          <>
+            <Header />
+            <ConsejosPadres />
+            <Footer />
+          </>
+        } />
+        <Route path="/nivelacion" element={
+          <>
+            <Header />
+            <NibelacionRecuperacion />
+            <Footer />
+          </>
+        } />
+        <Route path="/cursos-vacacionales" element={
+          <>
+            <Header />
+            <CursosVacacionales />
+            <Footer />
+          </>
+        } />
+        <Route path="/" element={
+          <>
+            <Header />
+            <Hero />
+            <ConsultasGrid />
+            <MatriculaSection />
+            <Testimonios />
+            <Blog />
+            <Footer />
+          </>
+        } />
+      </Routes>
+      <Chatbot2 />
+    </>
   )
 }
 
 export default App
+
